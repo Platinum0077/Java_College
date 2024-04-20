4a14a8afb4879ca585371d1c64fcf2c1ad8496dc
Pranjal Agrawal <139432325+Platinum0077@users.noreply.github.com>
2024-04-21 01:30:51 +0530

    Add files via upload

---
+++ PersonalDetails.java
@@ -0,0 +1,17 @@
+//This is the comment section.
+/*
+	This is multiline comment.
+	Program is to display Hello World
+*/
+
+class PersonalDetails {
+	public static void main(String[] args) {
+		/*
+		System.out.println("Name : Pranjal Arawal");
+		System.out.println("Roll No. : 22051868");
+		System.out.println("Section : CSE - 48");
+		System.out.println("Branch : CSE");
+		*/
+		System.out.println("Name : Pranjal Arawal" + "\n" + "Roll No. : 22051868");
+	}
+}
